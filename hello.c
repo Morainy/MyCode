3323a403c5156484a30d4f8908c057714ec25b05
Morain <morain@morain.(none)>
2015-01-22 12:57:51 +0800

    Test

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: Morain
+    > Mail: morainchen135@gmail.com 
+    > Created Time: 2015年01月22日 星期四 12时57分03秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc ,char * argv[])
+{
+	printf("Hello\n");
+}
